7356da19aeabfa3f32c5cfb58d8b84c8afb3ab5d
BlondeNamazu <namazutitech15@gmail.com>
2022-09-26 10:18:06 +0900

    consider calculation cost

--- fourth.kts
+++ fourth.kts
@@ -2,21 +2,25 @@ import java.io.File
 
 val wordList = File("dictionary.txt").readLines()
 
+// O(N)
 val lengthToWordMap = wordList.groupBy { word -> word.length }.mapValues { it.value.toSet() }
 
+fun isWordContained(word: String): Boolean {
+  return lengthToWordMap[word.length]?.contains(word) == true
+}
+
+// O(LNlog(N))
 fun divideWord(word: String) : List<Pair<String,String>> {
   return (1..word.length)
-    .filter { prefixLength ->
-      val prefix = word.substring(0, prefixLength)
-      lengthToWordMap[prefixLength]?.contains(prefix) == true
-    }.mapNotNull { prefixLength ->
+    .mapNotNull { prefixLength ->
       val prefix = word.substring(0, prefixLength)
       val suffix = word.substring(prefixLength)
-      if(lengthToWordMap[suffix.length]?.contains(suffix) == true) Pair(prefix, suffix)
+      if(isWordContained(prefix) && isWordContained(suffix)) Pair(prefix, suffix)
       else null
     }
 }
 
+// O(N) * divideWord = O(LN^2log(N))
 (lengthToWordMap[6] ?: emptyList<String>()).forEach { word ->
   val answers = divideWord(word)
   answers
@@ -24,4 +28,8 @@ fun divideWord(word: String) : List<Pair<String,String>> {
       println("${pair.first}${pair.second} = ${pair.first} + ${pair.second}")
     }
 }
+
+// 計算量
+// L: word length
+// N: number of words in dictionary
 
