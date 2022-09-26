9ac0412485b7bdc1a62fd50236fd38be609e1771
BlondeNamazu <namazutitech15@gmail.com>
2022-09-26 11:32:35 +0900

    :wip:

---
+++ succeed/third.kts
@@ -0,0 +1,37 @@
+import java.io.File
+
+val wordList = File("../dictionary.txt").readLines()
+
+val originalWordLength = 6
+
+// O(N)
+val lengthToWordMap = wordList.groupBy { word -> word.length }.mapValues { it.value.toSet() }
+
+fun isWordContained(word: String): Boolean {
+  return lengthToWordMap[word.length]?.contains(word) == true
+}
+
+// O(LNlog(N))
+fun divideWord(word: String) : List<Pair<String,String>> {
+  return (1..word.length)
+    .mapNotNull { prefixLength ->
+      val prefix = word.substring(0, prefixLength)
+      val suffix = word.substring(prefixLength)
+      if(isWordContained(prefix) && isWordContained(suffix)) Pair(prefix, suffix)
+      else null
+    }
+}
+
+// O(N) * divideWord = O(LN^2log(N))
+(lengthToWordMap[originalWordLength] ?: emptyList<String>()).forEach { word ->
+  val answers = divideWord(word)
+  answers
+    .forEach { pair ->
+      println("${pair.first}${pair.second} = ${pair.first} + ${pair.second}")
+    }
+}
+
+// 計算量
+// L: word length
+// N: number of words in dictionary
+
