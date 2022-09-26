99a0e9669320c38b2e8ed48d26d462092d037908
BlondeNamazu <namazutitech15@gmail.com>
2022-09-26 11:32:47 +0900

    :wip:

--- succeed/third.kts
+++ succeed/third.kts
@@ -3,6 +3,7 @@ import java.io.File
 val wordList = File("../dictionary.txt").readLines()
 
 val originalWordLength = 6
+va
 
 // O(N)
 val lengthToWordMap = wordList.groupBy { word -> word.length }.mapValues { it.value.toSet() }
@@ -21,6 +22,26 @@ fun divideWord(word: String) : List<Pair<String,String>> {
       else null
     }
 }
+
+// O(LNlog(N))
+// "hoge", 3 -> ("ho", ["g","e"])
+fun dividePrefix(word: String, divideCount: Int) : List<Pair<String,List<String>>> {
+  if (divideCount == 1) {
+    return if (isWordContained(word)) listOf(Pair(word, emptyList()))
+    else emptyList()
+  } else {
+    (1..word.length)
+      .mapNotNull { prefixLength
+        val prefix = word.substring(0, prefixLength)
+        return if (isWordContained(prefix)) {
+          val suffix = word.substring(prefixLength)
+          val ret = dividePrefix(suffix, divideCount - 1)
+          ret.map { (childPrefix, suffixList) ->
+            Pair(prefix, listOf(childPrefix).plus(suffixList))
+          }
+        } else null
+      }
+  }
 
 // O(N) * divideWord = O(LN^2log(N))
 (lengthToWordMap[originalWordLength] ?: emptyList<String>()).forEach { word ->
