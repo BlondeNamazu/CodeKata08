439654351d79257b1faf916b1c7e4c638b50a1ec
BlondeNamazu <namazutitech15@gmail.com>
2022-10-03 10:05:59 +0900

    third works

--- succeed/third.kts
+++ succeed/third.kts
@@ -3,37 +3,23 @@ import java.io.File
 val wordList = File("../dictionary.txt").readLines()
 
 val originalWordLength = 6
-va
 
-// O(N)
 val lengthToWordMap = wordList.groupBy { word -> word.length }.mapValues { it.value.toSet() }
 
 fun isWordContained(word: String): Boolean {
   return lengthToWordMap[word.length]?.contains(word) == true
 }
 
-// O(LNlog(N))
-fun divideWord(word: String) : List<Pair<String,String>> {
-  return (1..word.length)
-    .mapNotNull { prefixLength ->
-      val prefix = word.substring(0, prefixLength)
-      val suffix = word.substring(prefixLength)
-      if(isWordContained(prefix) && isWordContained(suffix)) Pair(prefix, suffix)
-      else null
-    }
-}
-
-// O(LNlog(N))
 // "hoge", 3 -> ("ho", ["g","e"])
 fun dividePrefix(word: String, divideCount: Int) : List<Pair<String,List<String>>> {
-  if (divideCount == 1) {
+  return if (divideCount == 1) {
     return if (isWordContained(word)) listOf(Pair(word, emptyList()))
     else emptyList()
   } else {
     (1..word.length)
-      .mapNotNull { prefixLength
+      .mapNotNull { prefixLength ->
         val prefix = word.substring(0, prefixLength)
-        return if (isWordContained(prefix)) {
+        if (isWordContained(prefix)) {
           val suffix = word.substring(prefixLength)
           val ret = dividePrefix(suffix, divideCount - 1)
           ret.map { (childPrefix, suffixList) ->
@@ -41,15 +27,20 @@ fun dividePrefix(word: String, divideCount: Int) : List<Pair<String,List<String>
           }
         } else null
       }
+      .flatten()
   }
+}
 
-// O(N) * divideWord = O(LN^2log(N))
 (lengthToWordMap[originalWordLength] ?: emptyList<String>()).forEach { word ->
-  val answers = divideWord(word)
-  answers
-    .forEach { pair ->
-      println("${pair.first}${pair.second} = ${pair.first} + ${pair.second}")
-    }
+  (2 until originalWordLength).forEach { length ->
+    val answers = dividePrefix(word, length)
+    answers
+      .forEach { pair ->
+        val list = listOf(pair.first).plus(pair.second)
+        list.forEach{ print("$it,") }
+        println()
+      }
+  }
 }
 
 // 計算量
